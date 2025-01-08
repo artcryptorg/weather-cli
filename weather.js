ebd284133485a32676469e550444e7418bf5d56c
keep <aleksprodolls@gmail.com>
2025-01-08 11:57:49 +0100

    api

--- weather.js
+++ weather.js
@@ -1,18 +1,33 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
+import { getWeather } from './services/api.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	if (!token.length) {
+		printError('Не передан токен');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
+		printSuccess('Токен сохранен')
+	} catch (e) {
+		printError(e.message);
+	}
+}
+
 const initCLI = () => {
 	const args = getArgs(process.argv)
-	console.log(args);
+	console.log(args)
 	if (args.h) {
-		//выводим хелп
+		printHelp();
 	}
 	if (args.s) {
-		//сохранить город
 	}
 	if (args.t) {
-		// сохранить токен
+		return saveToken(args.t)
 	}
-	// вывести погоду
+	getWeather('moscow');
 };
-
 initCLI();
